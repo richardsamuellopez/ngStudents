59e797ef0040b109d0eea4d4e7778449b31be250
richardsamuellopez <richardsameullopez@gmail.com>
2014-10-07 11:18:29 -0400

    Added JSON

--- src/ngStudents/StudentsResources.java
+++ src/ngStudents/StudentsResources.java
@@ -10,15 +10,56 @@ import java.sql.*;
 
 import ngStudents.jdbc.db.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import ngStudents.util.ToJSON;
+
+
 @Path("/v1/students")
 public class StudentsResources {
 
 	
 	// Return all students
 	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String returnTitle(){
-		return "<p>Java Web Service</p>";
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnAllStudents() throws Exception{
+		
+		PreparedStatement query = null;
+		Connection conn = null;
+		String returnString = null;
+		Response rb = null;
+		
+		try{
+			conn = JDBCMySQLConnection.getConnection();
+			query = conn.prepareStatement("select * from students");
+			ResultSet rs = query.executeQuery();
+			
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			query.close();
+			
+			returnString = json.toString();
+			rb = Response.ok(returnString).build();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			if(conn != null)conn.close();
+		}
+		
+		return rb;
 	}
 	
 	@Path("{id}")
